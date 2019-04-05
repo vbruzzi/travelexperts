987c239d4153ab2deb8411f68ee328a3bcdb904a
Samira456 <Samtleben.Samira@gmail.com>
2019-04-05 07:53:56 -0600

    functional code with 2 loops, no design

--- contacts.php
+++ contacts.php
@@ -64,70 +64,10 @@ function normalImg(x) {       //dimension of normal image, shown as default
               <h5>Where to find us in...</h5>
             </td>
             <td></td>
+        </tr>
+            <?php require "contactspull.php"?>
 </table>
-
-
-
-             <!-- start to work with function for agents here.-->
-<?php 
-                 // Start by printing command that will generate the beginning or the table. Place table for
-                 // Calgary in flex-container to organize page layout.
-     print("<div class='flex-container'><table class=Calgarytable> 
-            <tr>
-                <td class=headlinecell> Locations in Calgary
-                </td>
-            </tr>");
-// call the file with function that connects to database and pulls out the agency data
-    include("contactspull.php");
-    // agency data is in variable $result
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-    $resultagent = getRows("SELECT AgtLastName, AgtFirstName, AgtBusphone, AgtEmail from agents  where AgencyId=1 order by AgtFirstName");
-        if($resultagent != false) 
-             {   
-                 // use the while loop  to loop through all rows in SQL result table
-                 while ($row=mysqli_fetch_assoc($resultagent))
-                 {
-                    
-                     print("<tr><td class = Addresscell>Branch 1: ".$row["AgncyCity"]. "  ".$row["AgncyAddress"]."</td></tr>");
-                 }//close while
-             
-                 //print("</table>");
-            } //close if
-            
-    // end of agency pull
-    //insert and call the function that connects to database for agents-->
-        $result =getRows("SELECT AgtLastName, AgtFirstName, AgtBusphone, AgtEmail from agents  where AgencyId=1 order by AgtFirstName");//call the function
-        if($result != false) //check return value
-        {
-        print("<tr><td class = modalheading> Travelagents in Branch 1: ");
-        //step4, run the loop
-             while ($row=mysqli_fetch_assoc($result))
-            //step5, output html
-            {
-            print(" <tr><td><div class = modalagentname>".$row["AgtFirstName"]. " ".$row["AgtLastName"]."</div>Phone:".$row["AgtBusphone"]."<br/> e-mail:".$row["AgtEmail"]."</td></tr>");
-            } //end hwile
-        print("</table></div>");
-        } //end if
-
-
-
- require "scripts/footer.php" ?> 
+<?php require "scripts/footer.php" ?> 
 
 </body>
 </html>
